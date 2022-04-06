16e5aebeeb16ee1d3dc4e5d01e1d7ffa238a7747
dakotarawlings <dakotarawlings@gmail.com>
2022-04-06 13:22:37 -0700

    Youtube comment scraping script

--- youtube_comment_scraping.py
+++ youtube_comment_scraping.py
@@ -6,70 +6,113 @@ import spacy
 import time
 
 import requests
-import pandas as pd
+from urllib.parse import urlparse
 import json
 
 import pickle
 
+# URL='https://www.youtube.com/watch?v=Yk-unX4KnV4'
+# obj=urlparse(URL)
+# print(obj.query.replace('v=',''))
+
+
+
+def getComments(videoId, maxComments):
+        isPageToken=True
+        count=0
+        PageToken=''
+        corpus=[]
+        while count<maxComments and isPageToken==True:
+                URL="https://www.googleapis.com/youtube/v3/commentThreads"
+
+                parameters={
+                        'key': 'AIzaSyCH2b5Euatt-YmsXycpfRBtxtOUammZvL4',
+                        'part':'snippet',
+                        'videoId':videoId,
+                        'textFormat':'plainText',
+                        'maxResults':'100'
+                        }
+                if len(PageToken)>0:
+                        parameters['pageToken']=PageToken
+
+                r=requests.get(URL, params=parameters,)
+
+                jsonResponse=r.json()
+
+                corpus.extend([jsonResponse["items"][i]["snippet"]["topLevelComment"]["snippet"]["textDisplay"] for i in range(len(jsonResponse["items"]))])
+                
+                if 'nextPageToken' in jsonResponse:
+                        PageToken=jsonResponse['nextPageToken']
+                        isPageToken=True
+                else:
+                        isPageToken=False
+                count+=jsonResponse['pageInfo']['totalResults']
+        return corpus
+
+def getVideoCommentSentiments(videoURL): 
+
+        obj=urlparse(videoURL)
+        videoId=obj.query.replace('v=','')
+
+        corpus=getComments(videoId, 500)
+
+        nlp=spacy.load(r'C:\Users\dakot\anaconda3\envs\youtube_nlp\Lib\site-packages\en_core_web_lg\en_core_web_lg-3.2.0')
+
+        with nlp.disable_pipes():
+                vectors = np.array([nlp(comment).vector for comment in corpus])
+
+        df_vectors=pd.DataFrame(vectors)
+        #Test our serialized model before deployment
+        modelFile = open('model_svc.p', 'rb')     
+        pmodel = pickle.load(modelFile)
+        y_predp=pmodel.predict(df_vectors)
+        modelFile.close()
+
+        sentimentRatio=list(y_predp).count(1)/(list(y_predp).count(0)+list(y_predp).count(1))
+
+        return [y_predp, sentimentRatio]
 
 #%%
-def getComments(videoId, numComments):
-        URL="https://www.googleapis.com/youtube/v3/commentThreads"
+[y_predp_test, ratio]=getVideoCommentSentiments('https://www.youtube.com/watch?v=qfyynHBFOsM')
 
-        parameters={
-                'key': 'AIzaSyCH2b5Euatt-YmsXycpfRBtxtOUammZvL4',
-                'part':'snippet',
-                'videoId':videoId,
-                'textFormat':'plainText',
-                'maxResults':'100'
-                }
-        r=requests.get(URL, params=parameters,)
+print(len(y_predp_test))
 
-        jsonResponse=r.json()
 
-        corpus=[jsonResponse["items"][i]["snippet"]["topLevelComment"]["snippet"]["textDisplay"] for i in range(len(jsonResponse["items"]))]
+
+
 
 #%%
-
 URL="https://www.googleapis.com/youtube/v3/commentThreads"
 
 parameters={
         'key': 'AIzaSyCH2b5Euatt-YmsXycpfRBtxtOUammZvL4',
         'part':'snippet',
-        'videoId':'Yk-unX4KnV4',
+        'videoId':'qfyynHBFOsM',
         'textFormat':'plainText',
-        'maxResults':'10'
+        'maxResults':'100'
         }
+
+#parameters['pageToken']=PageToken
+
 r=requests.get(URL, params=parameters,)
 
 jsonResponse=r.json()
 
+print(jsonResponse)
+
 corpus=[jsonResponse["items"][i]["snippet"]["topLevelComment"]["snippet"]["textDisplay"] for i in range(len(jsonResponse["items"]))]
 
-#%%
-#Pass this into next api call
-print(jsonResponse['nextPageToken'])
-#print(json.dumps(jsonResponse["items"][19]["snippet"]["topLevelComment"]["snippet"]["textDisplay"], indent=4))
+jsonResponse['pageInfo']['totalResults']
 
-#%%
 
-nlp=spacy.load(r'C:\Users\dakot\anaconda3\envs\youtube_nlp\Lib\site-packages\en_core_web_lg\en_core_web_lg-3.2.0')
 
-with nlp.disable_pipes():
-    vectors = np.array([nlp(comment).vector for comment in corpus])
 
-#%%
-df_vectors=pd.DataFrame(vectors)
-#Test our serialized model before deployment
-modelFile = open('model_svc.p', 'rb')     
-pmodel = pickle.load(modelFile)
-y_predp=pmodel.predict(df_vectors)
-modelFile.close()
 
-#%%
 
-print(y_predp)
-print(list(y_predp).count(1)/(list(y_predp).count(0)+list(y_predp).count(1)))
+
+
+
+
 
 
 
