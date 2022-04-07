8393b0e9f69cf6536acbc115184292aadcb5ea8d
dakotarawlings <dakotarawlings@gmail.com>
2022-04-06 22:45:30 -0700

    Initialize WebApp files

---
+++ WebApplication/application.py
@@ -0,0 +1,46 @@
+
+#import flask tools
+from flask import Flask, jsonify, render_template, request
+
+#Import image an file processing tools
+
+import numpy as np
+import pandas as pd
+import pickle
+
+import get_youtube_comment_data
+
+#Call flask constructor
+
+app=Flask(__name__)
+
+#Define flask endpoint for the main html page
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+@app.route('/youtubeVideoSentiment', methods=[ 'GET', 'POST'])
+
+def temperatureForecast():
+    
+    if request.method=='POST':
+
+        request_json=request.get_json()
+
+        videoURL=request_json['URL']
+
+        #monitor the success of the API through a success attribute
+        response={'success': False}
+        [predictions, sentimentRatio]=get_youtube_comment_data.getVideoCommentSentiments(videoURL)
+        response['sentimentRatio']=sentimentRatio
+
+
+    return jsonify(response)
+
+if __name__=='__main__':
+    
+    app.run(debug=True)
+    
+
---
+++ get_youtube_comment_data.py
@@ -0,0 +1,118 @@
+#%%
+
+import numpy as np
+import pandas as pd
+import spacy
+import time
+
+import requests
+from urllib.parse import urlparse
+import json
+import pickle
+
+# URL='https://www.youtube.com/watch?v=Yk-unX4KnV4'
+# obj=urlparse(URL)
+# print(obj.query.replace('v=',''))
+
+
+
+def getComments(videoId, maxComments):
+        isPageToken=True
+        count=0
+        PageToken=''
+        corpus=[]
+        while count<maxComments and isPageToken==True:
+                URL="https://www.googleapis.com/youtube/v3/commentThreads"
+
+                parameters={
+                        'key': 'AIzaSyCH2b5Euatt-YmsXycpfRBtxtOUammZvL4',
+                        'part':'snippet',
+                        'videoId':videoId,
+                        'textFormat':'plainText',
+                        'maxResults':'100'
+                        }
+                if len(PageToken)>0:
+                        parameters['pageToken']=PageToken
+
+                r=requests.get(URL, params=parameters,)
+
+                jsonResponse=r.json()
+
+                corpus.extend([jsonResponse["items"][i]["snippet"]["topLevelComment"]["snippet"]["textDisplay"] for i in range(len(jsonResponse["items"]))])
+                
+                if 'nextPageToken' in jsonResponse:
+                        PageToken=jsonResponse['nextPageToken']
+                        isPageToken=True
+                else:
+                        isPageToken=False
+                count+=jsonResponse['pageInfo']['totalResults']
+        return corpus
+
+def getVideoCommentSentiments(videoURL): 
+
+        obj=urlparse(videoURL)
+        videoId=obj.query.replace('v=','')
+
+        corpus=getComments(videoId, 500)
+
+        nlp=spacy.load(r'C:\Users\dakot\anaconda3\envs\youtube_nlp\Lib\site-packages\en_core_web_lg\en_core_web_lg-3.2.0')
+
+        with nlp.disable_pipes():
+                vectors = np.array([nlp(comment).vector for comment in corpus])
+
+        df_vectors=pd.DataFrame(vectors)
+        #Test our serialized model before deployment
+        modelFile = open('model_svc.p', 'rb')     
+        pmodel = pickle.load(modelFile)
+        y_predp=pmodel.predict(df_vectors)
+        modelFile.close()
+
+        sentimentRatio=list(y_predp).count(1)/(list(y_predp).count(0)+list(y_predp).count(1))
+
+        return [y_predp, sentimentRatio]
+
+#%%
+[y_predp_test, ratio]=getVideoCommentSentiments('https://www.youtube.com/watch?v=qfyynHBFOsM')
+
+print(len(y_predp_test))
+
+
+
+
+
+#%%
+URL="https://www.googleapis.com/youtube/v3/commentThreads"
+
+parameters={
+        'key': 'AIzaSyCH2b5Euatt-YmsXycpfRBtxtOUammZvL4',
+        'part':'snippet',
+        'videoId':'qfyynHBFOsM',
+        'textFormat':'plainText',
+        'maxResults':'100'
+        }
+
+#parameters['pageToken']=PageToken
+
+r=requests.get(URL, params=parameters,)
+
+jsonResponse=r.json()
+
+print(jsonResponse)
+
+corpus=[jsonResponse["items"][i]["snippet"]["topLevelComment"]["snippet"]["textDisplay"] for i in range(len(jsonResponse["items"]))]
+
+jsonResponse['pageInfo']['totalResults']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
